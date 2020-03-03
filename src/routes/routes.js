c393ac5717d79309a46585396b73f835605723d5
Pedro Mendoza <pemimeco@gmail.com>
2020-03-03 12:48:18 -0400

    guardar fotos adultos fixed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -26,11 +26,15 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         const user = req.body
         // console.log(user.email)
-        if (user.nombre != '' || user.apat != '' || user.amat != '' || user.email != '' || user.telf != '' || user.ci != '') {
-            // if (validarImagen) {
-            cb(null, `${user.nombre} ${user.apat} ${user.amat}-${user.ci}.jpg`)
+        if (user.tipo == 'Adulto') {
+            cb(null, `${user.nombre} ${user.apat} ${user.amat}-${user.telf}.jpg`)
         } else {
-            cb('Error en el formulario.')
+            if (user.nombre != '' || user.apat != '' || user.amat != '' || user.email != '' || user.telf != '' || user.ci != '') {
+                // if (validarImagen) {
+                cb(null, `${user.nombre} ${user.apat} ${user.amat}-${user.ci}.jpg`)
+            } else {
+                cb('Error en el formulario.')
+            }
         }
     }
 })
@@ -127,25 +131,6 @@ router.get('/home/:menu', async function (req, res, next) {
     }
 });
 //----------------------------------------------
-
-async function buscarPersona(ci) {
-    await JSON.stringify(pool.query('SELECT * FROM personas WHERE ci=$1', [ci]));
-}
-async function insertarPersonas(nombres, apat, amat, email, telf, ci) {
-    await JSON.stringify(pool.query(`insert into personas (nombres,apat,amat,email,telf,ci,estado)
-                            values('${nombres}','${apat}','${amat}','${email}','${telf}','${ci}','T')`))
-
-}
-async function insertarVoluntarios(registro, idtipo, idpersona) {
-    await JSON.stringify(client.query(`insert into voluntarios (registro,idtipo,idpersona)
-                            values('${registro}','${idtipo}','${idpersona}')`))
-}
-async function insertarAdultos(idpersona) {
-    await JSON.stringify(client.query(`insert into voluntarios (idpersona)
-                            values('${idpersona}')`))
-}
-
-
 router.post('/agregar', async function (req, res, next) {
 
     uploadPhoto(req, res, async (err) => {
